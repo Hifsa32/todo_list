337398db62892949c462f6fc0133a3f741d08d44
Hifsa <hifsakhalid4321@gmail.com>
2025-09-16 17:10:56 -0600

    completed

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const todoForm = document.querySelector("form");
 const todoInput = document.getElementById("text-input");
 const todoListUL = document.getElementById("todo-list");
 
-let allTodos = [];
+let allTodos = getTodos();
+updateTodoList();
 
 todoForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -12,8 +13,13 @@ todoForm.addEventListener("submit", function (e) {
 function addTodo() {
   const todoText = todoInput.value.trim();
   if (todoText.length > 0) {
-    allTodos.push(todoText);
+    const todoObject = {
+      text: todoText,
+      completed: false,
+    };
+    allTodos.push(todoObject);
     updateTodoList();
+    saveTodos();
     todoInput.value = "";
   }
 }
@@ -29,10 +35,11 @@ function updateTodoList() {
 function createtodoItem(todo, todoIndex) {
   const todoId = "todo-" + todoIndex;
   const todoLI = document.createElement("li");
+  const todoText = todo.text;
   todoLI.className = "todo";
   todoLI.innerHTML = `
-      <input type="checkbox" id="${TodoId}" />
-      <label class="custom-checkbox" for="${TodoId}">
+      <input type="checkbox" id="${todoId}" />
+      <label class="custom-checkbox" for="${todoId}">
         <svg
           fill="transparent"
           xmlns="http://www.w3.org/2000/svg"
@@ -44,8 +51,8 @@ function createtodoItem(todo, todoIndex) {
           <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
         </svg>
       </label>
-      <label for="${TodoId}" class="todo-text">
-        ${todo}
+      <label for="${todoId}" class="todo-text">
+        ${todoText}
       </label>
 
       <button class="delete-button">
@@ -60,5 +67,31 @@ function createtodoItem(todo, todoIndex) {
           <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
         </svg>
       </button> `;
+  const deleteButton = todoLI.querySelector(".delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteTodoItem(todoIndex);
+  });
+  const checkbox = todoLI.querySelector("input");
+  checkbox.addEventListener("change", () => {
+    allTodos[todoIndex].completed = checkbox.checked;
+    saveTodos();
+  });
+  checkbox.checked = todo.completed;
   return todoLI;
+}
+
+function deleteTodoItem(todoIndex) {
+  allTodos = allTodos.filter((_, i) => i !== todoIndex);
+  saveTodos();
+  updateTodoList();
+}
+
+function saveTodos() {
+  const todoJson = JSON.stringify(allTodos);
+  localStorage.setItem("todos", todoJson);
+}
+
+function getTodos() {
+  const todos = localStorage.getItem("todos") || "[]";
+  return JSON.parse(todos);
 }
