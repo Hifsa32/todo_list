1b8cb912ed36a520209da83a0c07369c88e9e298
Hifsa <hifsakhalid4321@gmail.com>
2025-09-16 12:37:37 -0600

    added js

---
+++ script.js
@@ -0,0 +1,9 @@
+const todoForm = document.querySelector("form");
+const todoInput = document.getElementById("todo-text");
+const todoListUL = document.getElementById("todo-list");
+
+let allTodos = [];
+
+todoForm.addEventListener("submit", function (e) {
+  alert("test");
+});
