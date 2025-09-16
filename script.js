e12a4d3953bdd58491240601372387580f86ea3f
Hifsa <hifsakhalid4321@gmail.com>
2025-09-16 14:24:59 -0600

    added js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const todoForm = document.querySelector("form");
-const todoInput = document.getElementById("todo-text");
+const todoInput = document.getElementById("text-input");
 const todoListUL = document.getElementById("todo-list");
 
 let allTodos = [];
@@ -10,6 +10,55 @@ todoForm.addEventListener("submit", function (e) {
 });
 
 function addTodo() {
-  const todoText = todoInput.value;
-  alert(todoText);
+  const todoText = todoInput.value.trim();
+  if (todoText.length > 0) {
+    allTodos.push(todoText);
+    updateTodoList();
+    todoInput.value = "";
+  }
+}
+
+function updateTodoList() {
+  todoListUL.innerHTML = "";
+  allTodos.forEach((todo, todoIndex) => {
+    todoItem = createtodoItem(todo, todoIndex);
+    todoListUL.append(todoItem);
+  });
+}
+
+function createtodoItem(todo, todoIndex) {
+  const todoId = "todo-" + todoIndex;
+  const todoLI = document.createElement("li");
+  todoLI.className = "todo";
+  todoLI.innerHTML = `
+      <input type="checkbox" id="${TodoId}" />
+      <label class="custom-checkbox" for="${TodoId}">
+        <svg
+          fill="transparent"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#e3e3e3"
+        >
+          <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+        </svg>
+      </label>
+      <label for="${TodoId}" class="todo-text">
+        ${todo}
+      </label>
+
+      <button class="delete-button">
+        <svg
+          fill="var(--secondary-color)"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#e3e3e3"
+        >
+          <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+        </svg>
+      </button> `;
+  return todoLI;
 }
